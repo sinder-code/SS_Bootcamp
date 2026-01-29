82c7ee93614c936f1882f498026f1d40ccf49de9
sinder-code <sinder@nm-aist.ac.tz>
2026-01-29 11:59:19 +0300

    new files added
    These are important

---
+++ git_project/src/index.py
@@ -0,0 +1,15 @@
+print("version 3 feature 2 added")
+import time 
+for i in range(10):
+	print("i =",i)
+	time.sleep(2)
+
+def calVolume(w,l,h):
+	return (w*l + w*h + l*h)
+print(calVolume(5.5,6.5,4.5))
+
+dec calArea(a,b):
+	return a*b
+print(calArea(12,15))
+
+print(25)
